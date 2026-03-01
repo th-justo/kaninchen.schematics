6c1b10b44a029b00e0c89081b62e1e4e9d3b3317
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2026-03-01 01:14:09 -0300

    adjustment script

--- dist/component/index.d.ts
+++ dist/component/index.d.ts
@@ -1,1 +1,3 @@
-export declare function componentGenerator(): () => void;
+import { Rule } from '@angular-devkit/schematics';
+import { Schema } from './schema';
+export declare function componentGenerator(options: Schema): Rule;
--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,29 @@
-export function componentGenerator() {
-  return () => {};
+import { apply, mergeWith, move, Rule, SchematicContext, template, Tree, url, chain } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { Schema } from './schema';
+
+function renameTemplates(targetPath: string): Rule {
+  return (tree: Tree) => {
+    tree.visit(filePath => {
+      if (filePath.startsWith(targetPath) && filePath.endsWith('.template')) {
+        const newPath = filePath.replace('.template', '.ts'); // já vira .ts
+        tree.rename(filePath, newPath);
+      }
+    });
+    return tree;
+  };
+}
+
+export function componentGenerator(options: Schema): Rule {
+  const targetPath = options.path || 'src/app/components';
+
+  const sourceTemplates = apply(url('./files'), [
+    template({ ...options, ...strings }),
+    move(targetPath)
+  ]);
+
+  return chain([
+    mergeWith(sourceTemplates),
+    renameTemplates(targetPath)
+  ]);
 }
