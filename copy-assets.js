1c5da590b54d12c9629ffb5cdb710035f2d1df67
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2026-03-01 00:22:52 -0300

    adjustment

---
+++ copy-assets.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyRecursive(src, dest) {
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+
+  for (const item of fs.readdirSync(src)) {
+    const srcPath = path.join(src, item);
+    const destPath = path.join(dest, item);
+
+    if (fs.lstatSync(srcPath).isDirectory()) {
+      copyRecursive(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyRecursive('src', 'dist');
