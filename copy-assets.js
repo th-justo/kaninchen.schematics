3e4ad6b08ffd000a2494a2b28d3892c31225b838
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2026-03-01 00:33:54 -0300

    refactory

--- copy-assets.js
+++ copy-assets.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-function copyRecursive(src, dest) {
+function copyDir(src, dest) {
   if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
 
   for (const item of fs.readdirSync(src)) {
     const srcPath = path.join(src, item);
     const destPath = path.join(dest, item);
 
-    if (fs.lstatSync(srcPath).isDirectory()) {
-      copyRecursive(srcPath, destPath);
-    } else {
+    if (fs.statSync(srcPath).isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else if (!srcPath.endsWith('.ts')) {
       fs.copyFileSync(srcPath, destPath);
     }
   }
 }
 
-copyRecursive('src', 'dist');
+copyDir('src', 'dist');
+console.log('Assets copied to dist');
--- dist/component/index.ts
+++
@@ -1,3 +0,0 @@
-export function componentGenerator() {
-  return () => {};
-}
